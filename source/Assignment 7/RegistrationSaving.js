910268490e1823a3aec93a75086888d411ef67d7
shaalla <shanmukhaalla@gmail.com>
2016-03-10 04:48:28 -0600

    Assignment 7

---
+++ source/Assignment 7/RegistrationSaving.js
@@ -0,0 +1,33 @@
+
+
+var registrationap = angular.module("register",[]);
+
+registrationap.controller("RegistrationController", function($scope,$http, $httpParamSerializerJQLike){
+    
+$scope.SaveRegistration = function(first,last,email,phone,user,pass) {
+   console.log("inside login function");
+$http({
+    method: 'POST',
+    url : 'https://api.mongolab.com/api/1/databases/shanmuk/collections/students?apiKey=m-K-lcaCBR9mm8OtQc0TD0Y0Iq94Y40v',
+    data: JSON.stringify({
+                FirstName: first,
+                LastName: last,
+                Emailid: email,
+                Phone: phone,
+                UserName: user,
+                password: pass
+            }),
+    contentType: "application/json"
+}).success(function() {
+    $scope.first = "";
+    $scope.last = "";
+    $scope.email = "";
+    $scope.phone = "";
+    $scope.user = "";
+    $scope.pass = "";
+    
+    $scope.msg ="Registration Done";
+        })
+}
+    
+});
